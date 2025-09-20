5fe91ae59de9947e5771aed7b013e89b8164b296
ales27pm <56891139+ales27pm@users.noreply.github.com>
2025-09-20 07:19:32 -0700

    fix: update app setup signature for new rn

--- ios/MyOfflineLLMApp/AppDelegate.mm
+++ ios/MyOfflineLLMApp/AppDelegate.mm
@@ -8,7 +8,11 @@
 #import "RCTAppSetupUtils.h"
 #else
 #warning "RCTAppSetupUtils header not found. The app will skip RCTAppSetupPrepareApp; ensure Pods are installed if you rely on it."
-static inline void RCTAppSetupPrepareApp(id application) {}
+static inline void RCTAppSetupPrepareApp(id application, BOOL turboModuleEnabled)
+{
+  (void)application;
+  (void)turboModuleEnabled;
+}
 #endif
 
 @implementation AppDelegate
@@ -17,7 +21,7 @@ static inline void RCTAppSetupPrepareApp(id application) {}
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   // Prepare the React Native environment.
-  RCTAppSetupPrepareApp(application);
+  RCTAppSetupPrepareApp(application, [self turboModuleEnabled]);
   // Name must match the "name" field in app.json.
   self.moduleName = @"monGARS";
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
