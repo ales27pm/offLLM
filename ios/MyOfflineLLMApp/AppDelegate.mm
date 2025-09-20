65d961042d74830d7b0b98db910b916d78740fd2
ales27pm <56891139+ales27pm@users.noreply.github.com>
2025-09-20 06:42:42 -0700

    fix(ios): guard app setup utils include

--- ios/MyOfflineLLMApp/AppDelegate.mm
+++ ios/MyOfflineLLMApp/AppDelegate.mm
@@ -1,5 +1,15 @@
 #import "AppDelegate.h"
+
+#if __has_include(<React/RCTAppSetupUtils.h>)
 #import <React/RCTAppSetupUtils.h>
+#elif __has_include(<React_RCTAppDelegate/RCTAppSetupUtils.h>)
+#import <React_RCTAppDelegate/RCTAppSetupUtils.h>
+#elif __has_include("RCTAppSetupUtils.h")
+#import "RCTAppSetupUtils.h"
+#else
+#warning "RCTAppSetupUtils header not found. The app will skip RCTAppSetupPrepareApp; ensure Pods are installed if you rely on it."
+static inline void RCTAppSetupPrepareApp(id application) {}
+#endif
 
 @implementation AppDelegate
 
