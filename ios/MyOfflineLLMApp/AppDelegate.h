b2714775c9cacd05fd75aac4aac1345e455e6463
ales27pm <56891139+ales27pm@users.noreply.github.com>
2025-09-22 17:34:38 -0400

    Align AppDelegate imports with factory detection

--- ios/MyOfflineLLMApp/AppDelegate.h
+++ ios/MyOfflineLLMApp/AppDelegate.h
@@ -1,5 +1,13 @@
 #import <UIKit/UIKit.h>
 
+#if __has_include(<React-RCTAppDelegate/RCTDefaultReactNativeFactoryDelegate.h>)
+#import <React-RCTAppDelegate/RCTDefaultReactNativeFactoryDelegate.h>
+#elif __has_include(<React/RCTDefaultReactNativeFactoryDelegate.h>)
+#import <React/RCTDefaultReactNativeFactoryDelegate.h>
+#elif __has_include("RCTDefaultReactNativeFactoryDelegate.h")
+#import "RCTDefaultReactNativeFactoryDelegate.h"
+#endif
+
 #import "ReactNativeFactoryCompat.h"
 
 NS_ASSUME_NONNULL_BEGIN
@@ -7,6 +15,9 @@ NS_ASSUME_NONNULL_BEGIN
 #if RN_HAS_REACT_NATIVE_FACTORY
 @interface AppDelegate : RCTDefaultReactNativeFactoryDelegate <UIApplicationDelegate>
 #else
+@class RCTBridge;
+@protocol RCTBridgeDelegate;
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 #endif
 
