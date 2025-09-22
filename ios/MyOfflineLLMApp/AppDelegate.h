eb95a547c227a78295a64d3a922782cfbea7b581
ales27pm <56891139+ales27pm@users.noreply.github.com>
2025-09-22 17:18:01 -0400

    Refine AppDelegate delegate selection

--- ios/MyOfflineLLMApp/AppDelegate.h
+++ ios/MyOfflineLLMApp/AppDelegate.h
@@ -1,23 +1,17 @@
 #import <UIKit/UIKit.h>
 
-#if __has_include(<React/RCTAppDelegate.h>)
-#import <React/RCTAppDelegate.h>
-#elif __has_include(<React_RCTAppDelegate/RCTAppDelegate.h>)
-#import <React_RCTAppDelegate/RCTAppDelegate.h>
-#elif __has_include("RCTAppDelegate.h")
-#import "RCTAppDelegate.h"
-#endif
-
 #import "ReactNativeFactoryCompat.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface AppDelegate : RNAppDelegateBaseClass <RN_APP_DELEGATE_PROTOCOLS>
+#if RN_HAS_REACT_NATIVE_FACTORY
+@interface AppDelegate : RCTDefaultReactNativeFactoryDelegate <UIApplicationDelegate>
+#else
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+#endif
 
 @property (nonatomic, strong) UIWindow *window;
 
 @end
 
 NS_ASSUME_NONNULL_END
-
-#undef RN_APP_DELEGATE_PROTOCOLS
