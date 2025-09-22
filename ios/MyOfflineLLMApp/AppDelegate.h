60be23ad041ff271ef31fd8442aaa3cafe75e427
ales27pm <56891139+ales27pm@users.noreply.github.com>
2025-09-22 17:08:53 -0400

    Restore AppDelegate umbrella import guard

--- ios/MyOfflineLLMApp/AppDelegate.h
+++ ios/MyOfflineLLMApp/AppDelegate.h
@@ -1,10 +1,23 @@
 #import <UIKit/UIKit.h>
+
+#if __has_include(<React/RCTAppDelegate.h>)
+#import <React/RCTAppDelegate.h>
+#elif __has_include(<React_RCTAppDelegate/RCTAppDelegate.h>)
+#import <React_RCTAppDelegate/RCTAppDelegate.h>
+#elif __has_include("RCTAppDelegate.h")
+#import "RCTAppDelegate.h"
+#endif
+
 #import "ReactNativeFactoryCompat.h"
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface AppDelegate : RNAppDelegateBaseClass <RN_APP_DELEGATE_PROTOCOLS>
 
 @property (nonatomic, strong) UIWindow *window;
 
 @end
+
+NS_ASSUME_NONNULL_END
 
 #undef RN_APP_DELEGATE_PROTOCOLS
